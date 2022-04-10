414a569b68d7b394467c6bb102902616b9d13247
adityabhagat007 <adityabhagat707@gmail.com>
2022-04-10 16:29:20 +0530

    Added complete mylocation

--- src/component/MyLocation.js
+++ src/component/MyLocation.js
@@ -1,16 +1,55 @@
 import Loading from "./Loading";
+import { Card, Button ,Badge } from "react-bootstrap";
 
+const listCard = (props) =>{
+  console.log(props.data)
+  return(
+    <Card>
+        <Card.Header>{props.data.address}</Card.Header>
+        <Card.Body>
+          <Card.Title>Special title treatment</Card.Title>
+          <Card.Text>
+            With supporting text below as a natural lead-in to additional
+            content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+  )
+}
 
+const MyLocationData = (props) => {
+  console.log(props);
+  let unregistered= props.data.getUnavailableHospitals;
+  console.log(props.data.getUnavailableHospitals);
+  let registered = props.data.getAvailableHospitalsObj;
 
-const MyLocation = async () => {
-   return new Promise((resolve, reject) => {
-      const showPosition = (res) =>{
-         resolve(res);
-      }
-       if(navigator.geolocation){
-           navigator.geolocation.getCurrentPosition(showPosition);
-       }
-    });
+  return <>
+   {registered.length ? registered.map((ele)=>
+    (<Card>
+      <Card.Header>{ele.name}</Card.Header>
+      <Card.Body>
+        <Card.Title>{ele.address.name}</Card.Title>
+        <Card.Text>
+          {ele.display_name}
+        </Card.Text>
+        <Button variant="primary">Details</Button>
+      </Card.Body>
+    </Card>)) : null
+  }
+  {unregistered.length && unregistered.map((ele)=>{
+    return(<Card>
+      <Card.Header>{ele.name} <Badge bg="secondary" style={{float:'right'}}>Not Registered</Badge></Card.Header>
+      <Card.Body>
+        <Card.Title>{ele.address.name}</Card.Title>
+        <Card.Text>
+          {ele.display_name}
+        </Card.Text>
+        <Button variant="primary">Details</Button>
+      </Card.Body>
+    </Card>)
+  })}
+  </>;
 };
 
-export default MyLocation;
+export default MyLocationData;
--- src/pages/nearByHospitals.js
+++ src/pages/nearByHospitals.js
@@ -1,53 +1,67 @@
-import { useState, Fragment , useEffect, useReducer} from "react";
+import { useState,useEffect,} from "react";
 import { useParams } from "react-router-dom";
 import { http } from "../helper/Http";
 import apis from "../helper/Apis";
 import Layout from "../layout/Layout";
-import { Col, Container ,Row} from "react-bootstrap";
+import { Col, Container, Row, Dropdown } from "react-bootstrap";
+import MyLocationData from "../component/MyLocation";
 
-const NearByHospitals = () =>{
-  const {lat, long} = useParams();
-  const [rad ,setRad ] =useState(5000);
-  const [details ,setDetails] =useState([])
+const NearByHospitals = () => {
+  const { lat, long } = useParams();
+  const [rad, setRad] = useState(5);
+  const [details, setDetails] = useState(null);
 
-  useEffect(()=>{
+  useEffect(() => {
+    // alert("dsad")
     const fetchDeatails = async () => {
-      const res = await http(
-         `${apis.NEAR_BY_HOSPITAL}`,{
-           lat:lat,
-           long:long,
-           radius:rad
-         }
-      );
+
+      const res = await http(`${apis.NEAR_BY_HOSPITAL}`, {
+        lat: lat,
+        lon: long,
+        radius: +rad*1000,
+      });
       if (!res.data.status) {
         setDetails(null);
-     }
-     setDetails(res.data.data);
-   };
-   fetchDeatails();
-  },[lat,long])
-  console.log(details);
+      }
+      setDetails(res.data.data);
+    };
+    fetchDeatails();
+  }, [lat, long,rad]);
+  // console.log(details[0]);
+
   return (
     <Layout>
       <Container
-      style={{
-        marginTop:'30px'
-      }}
-       >
-         <Row>
-           <h1>See your NearBY Hospitals ....</h1>
-         </Row>
+        style={{
+          marginTop: "30px",
+        }}
+      >
+        <Row>
+          <h1>See your Nearest Hospitals ....</h1>
+          <hr></hr>
+        </Row>
         <Row>
           <Col sm={4}>
- 
+            <Dropdown onSelect={(value)=>{
+              setRad(value)
+            }}>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Dropdown Button
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item eventKey ={5}>5</Dropdown.Item>
+                <Dropdown.Item eventKey ={10}>10</Dropdown.Item>
+                <Dropdown.Item evenKey  = {15}>15</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </Col>
           <Col sm={8}>
-dsfsdfdsf
+            {details ? (<MyLocationData data={details ? details[0] : []}/>):null}
           </Col>
         </Row>
       </Container>
     </Layout>
-  )
-}
+  );
+};
 
-export default NearByHospitals
+export default NearByHospitals;
