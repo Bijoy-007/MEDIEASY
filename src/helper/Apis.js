51e631dda250e888f670bbd173e719c8d55bda90
Aritra Sen <aritrasen12345@gmail.com>
2022-12-13 20:06:52 +0530

    Change baseURL

--- src/helper/Apis.js
+++ src/helper/Apis.js
@@ -1,7 +1,7 @@
 let apis = {
-   baseUrl: "https://medieasy-backend-apis.herokuapp.com/api/v1",
-   SEARCH_PLACE: "/search_place",
-   GET_HOSPITAL_BY_PLACEID: "/hospital",
-   NEAR_BY_HOSPITAL:"/hospital/available",
+  baseUrl: "https://medieasy-backend.onrender.com/api/v1",
+  SEARCH_PLACE: "/search_place",
+  GET_HOSPITAL_BY_PLACEID: "/hospital",
+  NEAR_BY_HOSPITAL: "/hospital/available",
 };
 export default apis;
