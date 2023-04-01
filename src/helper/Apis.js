1c4329e75b67a6e760939f8dfafca8b97286aeb3
Aritra Sen <aritrasen12345@gmail.com>
2023-04-01 09:04:44 +0530

    Changed the baseUrl from render to cyclic

--- src/helper/Apis.js
+++ src/helper/Apis.js
@@ -1,5 +1,5 @@
 let apis = {
-  baseUrl: "https://medieasy-backend.onrender.com/api/v1",
+  baseUrl: "https://medieasy-backend.cyclic.app/api/v1/v1",
   SEARCH_PLACE: "/search_place",
   GET_HOSPITAL_BY_PLACEID: "/hospital",
   NEAR_BY_HOSPITAL: "/hospital/available",
